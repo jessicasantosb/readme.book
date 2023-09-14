24249a6aa3031e2fe55bfb27cb6a0c6525dd7d6c
jessicasantosb <jessica.santosb@outlook.com>
2023-09-14 11:33:26 -0300

    fix react build

--- frontend/src/components/protectedRoutes.js
+++ frontend/src/components/protectedRoutes.js
@@ -1,18 +1,21 @@
 import React from "react";
-import {useNavigate, Outlet, useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Cookies from "universal-cookie";
 
 function ProtectedRoutes() {
-
   const cookies = new Cookies();
   const Navigate = useNavigate();
-  const location = useLocation()
+  const location = useLocation();
+
   const token = cookies.get("token");
 
-  return (
-    token ? <Outlet />
-    : <Navigate to='/login' state={{from: location}} replace></Navigate>
+  token ? (
+    <Navigate to="/profile" state={{ from: location }} replace></Navigate>
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace></Navigate>
   );
+
+  return;
 }
 
-export default ProtectedRoutes
+export default ProtectedRoutes;
